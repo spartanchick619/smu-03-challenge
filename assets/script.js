79edaa7fcf011b48dfa18f3509ff8866aa336c0c
Rosa Valdez <rosa.m42@icloud.com>
2023-06-26 00:10:26 -0500

    Length prompt functional

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,9 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-    var password = generatePassword(17);
+function createPassword() {
+    var passwordLength = lengthPrompt();
+    var password = generatePassword(passwordLength);
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
@@ -20,9 +21,32 @@ function generatePassword(num) {
     return randomPassword;
 }
 
+function lengthPrompt() {
+    while (true) {
+        passwordLength = prompt("How many characters do you want your password to be?\nYou must choose a number between 8 and 128.");
+        if (passwordLength && isNaN(passwordLength)) {
+            alert("Invalid input.");
+        } else if (passwordLength) {
+            if (parseInt(passwordLength) >= 8 && parseInt(passwordLength) <= 128) {
+                return parseInt(passwordLength);
+            } else {
+                alert("You must choose a value between 8 and 128 inclusive.")
+            }
+        } else if (isNull(passwordLength)) {
+            return;
+        } else {
+            alert("Invalid input.");
+        }
+    }
+}
+
+function characterTypePrompt() {
+    return;
+}
+
 function getRandomCharacter() {
     return String.fromCharCode(Math.floor(Math.random() * 77) + 34);
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", createPassword);
